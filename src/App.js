13ff58059aa79122c1549f44169843a69bf7600e
Lohithm11 <lmundlap11@gmail.com>
2024-08-20 12:20:43 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+//import TaskManager from "./components/Task Manager/taskManager/TaskManager";
+import TaskManagerReducer from "./components/Task Manager/taskManager/TaskMangerReducer";
+
+function App() {
+  return (
+    <div>
+      <TaskManagerReducer /> 
+     
+    </div>
+  );
+};
+
+export default App;
